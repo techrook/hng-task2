3c80c42e7c948d22febf7197127a896567f15246
techrook <mondayitohowo3@gmail.com>
2024-07-05 16:23:44 +0100

    addede users and organisation validators

---
+++ src/organisation/dto/create-organisation.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateOrganisationDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  description?: string;
+}
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  phone?: string;
+}
